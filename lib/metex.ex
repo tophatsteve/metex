209f4ae7fc4caeba00f5bae800c3979f01ae1932
Steve Bentley <steve.bentley@chorus.co>
2017-10-03 10:29:25 +0100

    Add coordinator

--- lib/metex.ex
+++ lib/metex.ex
@@ -1,18 +1,11 @@
 defmodule Metex do
-  @moduledoc """
-  Documentation for Metex.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Metex.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def temperature_of(cities) do
+    coordinator_pid = spawn(Metex.Coordinator, :loop, [[], Enum.count(cities)])
+    cities |> Enum.each(fn city ->
+      worker_pid = spawn(Metex.Worker, :loop, [])
+      send worker_pid, {coordinator_pid, city}
+    end)
   end
+
 end
--- lib/worker.ex
+++ lib/worker.ex
@@ -1,55 +1,55 @@
 defmodule Metex.Worker do
-    
-    def loop do
-        receive do
-            {sender_pid, location} ->
-                send(sender_pid, {:ok, temperature_of(location)})
-            msg -> 
-                IO.puts "don't know how to process this message - #{msg}"
-        end
-        loop()
-    end
 
-    defp temperature_of(location) do
-        result = location |> url_for |> HTTPoison.get |> parse_response
-        case result do
-            {:ok, temp} ->
-                "#{location}: #{temp}C"
-            {:error, reason} ->
-                "#{location} error: #{reason}"                
-            :error ->
-                "some error"
-        end
+  def loop do
+    receive do
+      {sender_pid, location} ->
+        send(sender_pid, {:ok, temperature_of(location)})
+      msg -> 
+        IO.puts "don't know how to process this message - #{msg}"
     end
+    loop()
+  end
 
-    defp url_for(location) do
-        location = URI.encode(location)
-        "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey()}"
+  defp temperature_of(location) do
+    result = location |> url_for |> HTTPoison.get |> parse_response
+    case result do
+      {:ok, temp} ->
+        "#{location}: #{temp}C"
+      {:error, reason} ->
+        "#{location} error: #{reason}"                
+      :error ->
+      "some error"
     end
+  end
 
-    defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
-        body |> JSON.decode! |> compute_temperature
-    end
+  defp url_for(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey()}"
+  end
 
-    defp parse_response({:ok, %HTTPoison.Response{status_code: 404}}) do
-        {:error, "not found"}
-    end
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode! |> compute_temperature
+  end
 
-    defp parse_response(_) do
-        :error
-    end
+  defp parse_response({:ok, %HTTPoison.Response{status_code: 404}}) do
+    {:error, "not found"}
+  end
 
-    defp compute_temperature(json) do
-        try do
-            temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
-            {:ok, temp}
-        rescue
-            _ -> :error
-        end
-    end
+  defp parse_response(_) do
+    :error
+  end
 
-    defp apikey do
-        "5a513c30007f4555987383dc50d544c4"
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
     end
+  end
+
+  defp apikey do
+    "5a513c30007f4555987383dc50d544c4"
+  end
 
 end
